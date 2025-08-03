4c2b1bd528bb4dd35564d85b468e0957f5fb91a7
Facelles <122049357+Facelles@users.noreply.github.com>
2025-08-03 18:09:42 +0300

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { useState } from 'react';
 import Tracker_IP from './components/Tracker_IP'
 
+
 function App() {
+  const [ipValue, setIpValue] = useState<string>("8.8.8.8");
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIpValue(e.target.value);
+  };
 
-
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <>
@@ -13,9 +21,12 @@ function App() {
 
           <h1 className='text-3xl font-bold text-white mb-5'> IP Address Tracker </h1>
 
-          <form action="" className='flex items-center rounded overflow-hidden w-110'>
+          <form onSubmit={handleSubmit} className='flex items-center rounded overflow-hidden w-110'>
             <input type="text" id="ip-tracker-input" placeholder='Search for any IP address or domain'
-             className='flex-grow pl-3 py-2 outline-none bg-white text-gray-400 placeholder-gray-400 border-none'/>
+             className='flex-grow pl-3 py-2 outline-none bg-white text-gray-400 placeholder-gray-400 border-none'
+             value={ipValue}
+             onChange={handleInputChange}
+             />
             <button type='submit' className='flex-shrink-0 px-3 py-2 bg-black hover:bg-purple-300'>
               <img src="/public/icon-arrow.svg" alt="icon" className='w-6 h-6' />
 
@@ -25,7 +36,7 @@ function App() {
         </header>
 
         <section>
-          <Tracker_IP></Tracker_IP>
+          <Tracker_IP ipValue={ipValue} onChange={setIpValue} />
         </section>
         
         <section className='flex flex-col items-center justify-center bg-cover bg-center bg-gray-200 min-h-screen w-full' />
--- src/components/Tracker_IP.tsx
+++ src/components/Tracker_IP.tsx
@@ -4,37 +4,87 @@ interface Location {
     country: string;
     region: string;
     city: string;
-    postalCode: string;
-    timezone: string;
+    postalCode?: string;
+    timezone?: string;
 }
 
 interface asInfo {
-    name: string;
-    domain: string;
+    name?: string;
+    domain?: string;
 }
 
 interface SearchIp {
     ip: string;
-    location: Location[];
-    sIfno: asInfo[];
+    location: Location;
+    asIfno: asInfo;
+}
+
+interface InputProps {
+  ipValue: string;
+  onChange: (ip: string) => void;
 }
 
 
 const API_KEY_IPIFY = import.meta.env.VITE_IPIFY_API_KEY;
 
 
-const Tracker_IP = () => {
+const Tracker_IP: React.FC<InputProps> = ({ ipValue, onChange }) => {
   
 const [ipData, setIpData] = useState<SearchIp | null>(null)
+const [error, setError] = useState<string | null>(null)
+const [loading, setLoading] = useState<boolean>(true)
+
 
 useEffect(() => {
-    // TODO: fetch GET from API 
-}, [])
+        const fetchFromAPI = async () => {
+        try{
+            setLoading(true);
+            const response = await fetch(
+                `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY_IPIFY}&ipAddress=${ipValue}`
+            );
+                const data = await response.json();
+
+                if(!data.ip || !data.location || !data.as){
+                    throw new Error('Invalide API Respone');
+                }
+
+                const parsedData: SearchIp = {
+                    ip: data.ip,
+                    location: {
+                        country: data.location.country,
+                        region: data.location.region,
+                        city: data.location.city,
+                        postalCode: data.location.postalCode,
+                        timezone: data.location.timezone,
+                    },
+                    asIfno: {
+                        name: data.as.name,
+                        domain: data.as.domain,
+                    },
+                };
+
+                setIpData(parsedData);
+                setError(null);
+            }  
+            catch(e){
+                console.error('Failde to fetch IP data: ', e);
+                setError("Error get info from API! ");
+                setIpData(null);
+            } finally{
+                setLoading(false);
+            }
+        };
+        if(ipValue){
+            fetchFromAPI();
+        }
+}, [ipValue])
 
 
   return (
     <div>
-        <p>{ipData?.ip}</p>
+        {ipData && (
+            <p>{ipData.ip}</p>
+        )}
     </div>
   )
 }
